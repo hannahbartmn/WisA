45f34e13d21d69bf6e4469d44068e9b31772679f
MaikeBr0 <119356623+MaikeBr0@users.noreply.github.com>
2023-02-14 17:43:02 +0100

    Update Funktionen_Skript_1.R
    Eine Funktion für Teil f wurde hinzugefügt, ist jedoch nicht ganz vollständig

--- Funktionen_Skript_1.R
+++ Funktionen_Skript_1.R
@@ -70,8 +70,114 @@ names(rgabe) <- c(names(table(dich))[1], names(table(dich))[2])
 return(rgabe) # Ausgabe der beiden Summaries
   }
 
+
+
+
+
+
+
+
   
   
+##f)
+
+# es werden Visualisierungen fuer drei Variablen erstellt. Dabei werden zwei Variablen fuer die Achsen verwendet und die dritte durch unterschiedliche
+# Farben gezeigt. Die Positionen der drei Variablen sind dabei fest gewaehlt. Man kann nur unterscheiden welche drei von Mathe_Int, Prog_Int, Studienfach und
+# Mathe_LK man gleichzeitig angezeigt werden sollen. Genutzt wird dabei das Paket "ggplot2". 
+
+
+# noch nicht fertige Funktion, jedoch die Visualisierung fuer 3 Variablen
+
+# muss vorher geladen
+install.packages("ggplot2")
+library(ggplot2)
+
+Visual_data <- function(data = data, n = 3, Int_Mathe = TRUE, Int_Prog = TRUE, data_Studiengang = TRUE, MatheLK = FALSE){
+  if(n == 3 & Int_Mathe == TRUE & Int_Prog == TRUE & data_Studiengang == TRUE & MatheLK == FALSE){
+    print(ggplot(data,                                                                          # ggplot wird vom Datensatz erstellt
+           aes(y = factor(Mathe_Int,                                                      # Interesse für Mathe auf y-Achse
+                          labels = c("sehr geringes Interesse = 1",                       # Beschriftung für die Punkte
+                                     "2", "3", "4", "5", "6",
+                                     "sehr hohes Interesse = 7")), 
+               x = Prog_Int,                                                              # auf die x-Achse kommt Interesse für Programmieren
+               color = Studienfach)) +                                                    # es wird farblich nach Studienfach unterschieden
+      geom_jitter(alpha = 1,                                                              # Farbstaerke und Groesse der Punkte im Gitter
+                  size = 1.5) + 
+      labs(title = "Interesse nach Studienfach",                                          # Die jeweiligen Achsen werden beschriftet
+           x = "Interesse an Programmiern", 
+           y = "Interesse an Mathe", las = 1) )
+  }
+  if(n == 3 & Int_Mathe == TRUE & Int_Prog == TRUE & data_Studiengang == FALSE & MatheLK == TRUE){
+    print( ggplot(data,                                                                          # ggplot wird vom Datensatz erstellt
+           aes(y = factor(Mathe_Int,                                                      # Interesse für Mathe auf y-Achse
+                          labels = c("sehr geringes Interesse = 1",                       # Beschriftung für die Punkte
+                                     "2", "3", "4", "5", "6",
+                                     "sehr hohes Interesse = 7")), 
+               x = Prog_Int,                                                              # auf die x-Achse kommt Interesse für Programmieren
+               color = Mathe_LK)) +                                                    # es wird farblich nach Mathe LK unterschieden
+      geom_jitter(alpha = 1,                                                              # Farbstaerke und Groesse der Punkte im Gitter
+                  size = 1.5) + 
+      labs(title = "Interesse nach Mathe LK",                                          # Die jeweiligen Achsen werden beschriftet
+           x = "Interesse an Programmiern", 
+           y = "Interesse an Mathe", las = 1) )
+  }
+  if(n == 3 & Int_Mathe == TRUE & Int_Prog == FALSE & data_Studiengang == TRUE & MatheLK == TRUE){
+    print( ggplot(data,                                                                          # ggplot wird vom Datensatz erstellt
+           aes(y = factor(Mathe_Int,                                                      # Interesse für Mathe auf y-Achse
+                          labels = c("sehr geringes Interesse = 1",                       # Beschriftung für die Punkte
+                                     "2", "3", "4", "5", "6",
+                                     "sehr hohes Interesse = 7")), 
+               x = Studienfach,                                                              # auf die x-Achse kommt das Studienfach
+               color = Mathe_LK)) +                                                    # es wird farblich nach Mathe LK unterschieden
+      geom_jitter(alpha = 1,                                                              # Farbstaerke und Groesse der Punkte im Gitter
+                  size = 1.5) + 
+      labs(title = "Mathe LK nach Studenfach und Int. Mathe",                                          # Die jeweiligen Achsen werden beschriftet
+           x = "Studienfach", 
+           y = "Interesse an Mathe", las = 1))
+  }
+  if(n == 3 & Int_Mathe == FALSE & Int_Prog == TRUE & data_Studiengang == TRUE & MatheLK == TRUE){
+    print(ggplot(data,                                                                          # ggplot wird vom Datensatz erstellt
+           aes(y = factor(Prog_Int,                                                      # Interesse für Programmieren auf y-Achse
+                          labels = c("sehr geringes Interesse = 1",                       # Beschriftung für die Punkte
+                                     "2", "3", "4", "5", "6",
+                                     "sehr hohes Interesse = 7")), 
+               x = Studienfach,                                                              # auf die x-Achse kommt das Studienfach
+               color = Mathe_LK)) +                                                    # es wird farblich nach Mathe LK unterschieden
+      geom_jitter(alpha = 1,                                                              # Farbstaerke und Groesse der Punkte im Gitter
+                  size = 1.5) + 
+      labs(title = "Mathe LK bei Studienfach und Int. Programmieren",                                          # Die jeweiligen Achsen werden beschriftet
+           x = "Studienfach", 
+           y = "Interesse an Programmieren", las = 1) )
+  }
+  if(n==4 & Int_Mathe == TRUE & Int_Prog == TRUE & data_Studiengang == TRUE & MatheLK == TRUE){
+    # Hier muss der Code fuer vier Variablen rein
+  }
+# Hier werden die Warnings geschrieben, falls die Variablen unzulaessig sind  
+  
+  # bei den Fall das n=4 ist, wird eine Warnung gegeben, falls nicht alle vier Folgevariablen TRUE gesetzt wurden
+  if(n == 4 & !(Int_Mathe == TRUE & Int_Prog == TRUE & data_Studiengang == TRUE & MatheLK == TRUE )){    
+    warning("Bei n=4 muessen alle vier Variablen auf TRUE gesetzt werden")
+  }
+  # bei den Fall das n=3 ist:
+  # falls vier statt drei Variablen TRUE gesetzt wurden
+  if(n == 3 & Int_Mathe == TRUE & Int_Prog == TRUE & data_Studiengang == TRUE & MatheLK == TRUE){
+    warning("Bei n=3 duerfen nur drei Variablen TRUE sein")
+  }
+  # falls zu wenig Variablen TRUE gesetzt wurden
+  if(n== 3 & (Int_Mathe == FALSE & Int_Prog == FALSE & data_Studiengang == TRUE & MatheLK == TRUE | Int_Mathe == FALSE & Int_Prog == TRUE & data_Studiengang == FALSE & MatheLK == TRUE |
+              Int_Mathe == FALSE & Int_Prog == TRUE & data_Studiengang == TRUE & MatheLK == FALSE | Int_Mathe == TRUE & Int_Prog == FALSE & data_Studiengang == FALSE & MatheLK == TRUE | 
+              Int_Mathe == TRUE & Int_Prog == FALSE & data_Studiengang == TRUE & MatheLK == FALSE | Int_Mathe == TRUE & Int_Prog == TRUE & data_Studiengang == FALSE & MatheLK == FALSE |
+              Int_Mathe == FALSE & Int_Prog == FALSE & data_Studiengang == FALSE & MatheLK == FALSE | Int_Mathe == FALSE & Int_Prog == FALSE & data_Studiengang == FALSE & MatheLK == TRUE |
+              Int_Mathe == FALSE & Int_Prog == FALSE & data_Studiengang == TRUE & MatheLK == FALSE | Int_Mathe == FALSE & Int_Prog == TRUE & data_Studiengang == FALSE & MatheLK == FALSE |
+              Int_Mathe == TRUE & Int_Prog == FALSE & data_Studiengang == FALSE & MatheLK == FALSE )){
+    warning("Bei n=3 muessen genau drei Variablen TRUE sein")
+  }
+  
+  # falls das n selbst unzulaessig ist
+  if(n != 3 & n!= 4){
+    warning("Das n ist unzulaessig. Es wird nur n=3 oder n=4 angenommen")
+  }
+ }
 
 
 
