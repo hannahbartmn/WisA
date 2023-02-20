fd642ad0cd38f2f7e42275a07114f8f086df8d77
hannahbartmn <119402984+hannahbartmn@users.noreply.github.com>
2023-02-20 12:12:35 +0100

    Update Auswertung_Datensatz.R

--- Auswertung_Datensatz.R
+++ Auswertung_Datensatz.R
@@ -4,5 +4,23 @@ library (readr)
 urlfile <- "https://raw.githubusercontent.com/hannahbartmn/WisA/main/data.csv"
 
 mydata<-read.csv(file = urlfile, sep = ";", header = TRUE)
-mydata
-View(mydata)
+D <- mydata
+View(D)
+
+#####
+# erstes Angucken des Datensatz: 
+
+str(D)
+# Der Datensatz enthaelt die sechs Variabeln ID (int), Alter (int), Studienfach (chr), Mathe_Int (int), Prog_Int (int), Mathe_LK (chr)
+# Alter ist metrisch: Funktion a, FUnktion d, Funktion e
+table(D$Studienfach)
+# Studienfach ist nominal mit Auspraegungen Data Science, Informatik, Mathe und Statistik: Funktionen b, c, f
+table(D$Mathe_Int)
+table(D$Prog_Int)
+# Mathe_Int und Prog_Int sind beide metrisch auf einer Skala von 1 bis 7: Funktionen a, b, c, d, e
+# 1 = sehr geringes Interesse, und 7 = sehr hohes Interesse
+table(D$Mathe_LK)
+# Mathe_LK ist dichotom nominalskaliert: b, c, d, f
+
+#####
+My_Plots(D$Mathe_Int)
